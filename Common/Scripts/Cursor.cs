e59d7c2b7b22183d9199a0494bc6eb5c75e9f688
Rico <rico@pgml.de>
2025-01-20 14:44:43 +0100

    reimplemented custom cursor

---
+++ Common/Scripts/Cursor.cs
@@ -0,0 +1,84 @@
+using Godot;
+
+public partial class Cursor : Node3D
+{
+	[Export]
+	public Texture2D Default;
+
+	[Export]
+	public Texture2D Hand;
+
+	[Export]
+	public Texture2D Grab;
+
+	//private ActorData _actorData;
+	//private AnimatedSprite3D _throw;
+	//private ContainerResource _container;
+
+	public override void _Ready()
+	{
+		//_actorData = Load<ActorData>(Resources.ActorData);
+		//_throw = GetNode<AnimatedSprite3D>("ThrowCursor");
+
+		CursorDefault();
+		//_container = Load<ContainerResource>(Resources.Container);
+	}
+
+	public override void _Process(double delta)
+	{
+		//if (_container.IsOpen) {
+		//	CursorDefault();
+		//}
+		//else if (_actorData.IsPickingUpItem) {
+		//	CursorGrab();
+		//}
+		//else if (_actorData.CanPickupItem || _actorData.CanOpenContainer) {
+		//	CursorHand();
+		//}
+		//else if (_actorData.CanThrowItem) {
+		//	CursorThrow();
+		//}
+		//else {
+			CursorDefault();
+		//}
+	}
+
+	public void CursorDefault()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+		Input.SetCustomMouseCursor(Default);
+		_hideThrowCursor();
+	}
+
+	public void CursorHand()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+		Input.SetCustomMouseCursor(Hand);
+		_hideThrowCursor();
+	}
+
+	public void CursorGrab()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+		Input.SetCustomMouseCursor(Grab);
+		_hideThrowCursor();
+	}
+
+	public void CursorThrow()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Hidden;
+		//_showThrowCursor();
+		//_throw.GlobalPosition = GetGlobalMousePosition();
+	}
+
+	private void _showThrowCursor()
+	{
+		//_throw.Visible = true;
+	}
+
+	private void _hideThrowCursor()
+	{
+		//_throw.Visible = false;
+	}
+}
+

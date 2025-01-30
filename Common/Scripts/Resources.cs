d3f9dc6ee1f0db09e213b4674542af56f9c597d1
Rico <rico@pgml.de>
2025-01-30 01:42:58 +0100

    fixed resources path

--- Common/Scripts/Resources.cs
+++ Common/Scripts/Resources.cs
@@ -5,11 +5,11 @@ public partial class Resources : Node3D
 	// Godot resources
 	public static readonly string ActorData = "res://Creatures/actor_data.tres";
 	public static readonly string AiData = "res://Creatures/ai_data.tres";
-	public static readonly string AudioLibrary = "res://Common/audio_library.tres";
+	public static readonly string AudioLibrary = "res://Common/Tools/audio_library.tres";
 
 	// Scenes
-	public static readonly string AudioInstance = "res://Common/audio_instance.tscn";
-	public static readonly string SceneTransition = "res://Common/scene_transition.tscn";
+	public static readonly string AudioInstance = "res://Common/Tools/audio_instance.tscn";
+	public static readonly string SceneTransition = "res://Common/Tools/scene_transition.tscn";
 
 	// UI
 	public static readonly string UiLoading = "res://UI/ui_loading.tscn";

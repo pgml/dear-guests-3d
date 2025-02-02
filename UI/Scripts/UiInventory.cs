682df0e3347fb72d265708940779ebcd543e8433
Rico <rico@pgml.de>
2025-02-02 17:54:41 +0100

    inventory: added toggle tab function

--- UI/Scripts/UiInventory.cs
+++ UI/Scripts/UiInventory.cs
@@ -1,6 +1,23 @@
 using Godot;
-using System;
 
 public partial class UiInventory : Control
 {
+	[Export]
+	public Panel InventoryBackgroud { get; set; }
+
+	public bool IsOpen { get; set; } = false;
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("toggle_inventory")) {
+			_toggleInventory();
+		}
+	}
+
+	private void _toggleInventory()
+	{
+		float posY = !IsOpen ? 0 : Size.Y;
+		Position = new Vector2(0, posY);
+		IsOpen = !IsOpen;
+	}
 }

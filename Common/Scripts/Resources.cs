6428c1f1b8e1139ff0b19dd72da529be1366dfa1
Rico <rico@pgml.de>
2025-02-03 02:02:53 +0100

    updated resources class

--- Common/Scripts/Resources.cs
+++ Common/Scripts/Resources.cs
@@ -1,6 +1,4 @@
-using Godot;
-
-public partial class Resources : Node3D
+public static class Resources
 {
 	// Godot resources
 	public static readonly string ActorData = "res://Creatures/actor_data.tres";
@@ -10,10 +8,12 @@ public partial class Resources : Node3D
 	public static readonly string DateTime = "res://Common/date_time.tres";
 	public static readonly string Console = "res://Common/console.tres";
 
-	// Scenes
+	// Tools
 	public static readonly string AudioInstance = "res://Common/Tools/audio_instance.tscn";
 	public static readonly string SceneTransition = "res://Common/Tools/scene_transition.tscn";
+	public static readonly string UseIndicator = "res://Common/Tools/use_indicator.tscn";
 
 	// UI
 	public static readonly string UiLoading = "res://UI/ui_loading.tscn";
+	public static readonly string QuickInventory = "res://UI/ui_quick_inventory.tscn";
 }

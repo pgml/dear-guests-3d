0ef54f18152096c85dfcac37ffb69b55b13ef54a
Rico <rico@pgml.de>
2025-02-05 01:17:01 +0100

    inventory: put item amount into separate column

--- UI/Scripts/UiBackPackItemList.cs
+++ UI/Scripts/UiBackPackItemList.cs
@@ -6,6 +6,7 @@ public partial class UiBackPackItemList : UiItemList
 {
 	public Dictionary<string, TitleProperties> ColumnTitles { get; set; } = new() {
 		{ "Item Name", new TitleProperties(HorizontalAlignment.Left, 90)},
+		{ "Amount", new TitleProperties(HorizontalAlignment.Right, 25) },
 		//{ "Weight", new TitleProperties(HorizontalAlignment.Right, 25) },
 		{ "Value", new TitleProperties(HorizontalAlignment.Right, 25) },
 	};
@@ -43,16 +44,18 @@ public partial class UiBackPackItemList : UiItemList
 			ItemResource item = inventoryItem.ItemResource;
 			TreeItem row = CreateItem(TreeRoot);
 			string amount = inventoryItem.Amount > 0
-				? $" ({inventoryItem.Amount.ToString()})"
-				: "";
+				//? $" ({inventoryItem.Amount.ToString()})"
+				? inventoryItem.Amount.ToString()
+				: "0";
 
 			//double weight = Math.Round(item.Weight * inventoryItem.Amount, 2);
 
-			row.SetText(0, $"{item.Name}{amount}");
+			row.SetText(0, item.Name);
 			//row.SetText(1, weight.ToString());
-			row.SetText(1, item.Value.ToString());
-			//row.SetTextAlignment(1, HorizontalAlignment.Right);
+			row.SetText(1, amount);
+			row.SetText(2, item.Value.ToString());
 			row.SetTextAlignment(1, HorizontalAlignment.Right);
+			row.SetTextAlignment(2, HorizontalAlignment.Right);
 
 			ListItems.Add(inventoryItem, row);
 		}

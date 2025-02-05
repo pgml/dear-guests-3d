5bf2b322ede6d1a1cd0004b96d4ba5943454c864
Rico <rico@pgml.de>
2025-02-05 13:12:15 +0100

    UI: Added inventory list item icon placeholder

--- UI/Scripts/UiBackPackItemList.cs
+++ UI/Scripts/UiBackPackItemList.cs
@@ -5,10 +5,11 @@ using System.Collections.Generic;
 public partial class UiBackPackItemList : UiItemList
 {
 	public Dictionary<string, TitleProperties> ColumnTitles { get; set; } = new() {
-		{ "Item Name", new TitleProperties(HorizontalAlignment.Left, 90)},
-		{ "Amount", new TitleProperties(HorizontalAlignment.Right, 25) },
+		{ "Item", new TitleProperties(HorizontalAlignment.Left, 0)},
+		{ "", new TitleProperties(HorizontalAlignment.Left, 80)},
+		{ "Amount", new TitleProperties(HorizontalAlignment.Right, 20) },
 		//{ "Weight", new TitleProperties(HorizontalAlignment.Right, 25) },
-		{ "Value", new TitleProperties(HorizontalAlignment.Right, 25) },
+		{ "Value", new TitleProperties(HorizontalAlignment.Right, 20) },
 	};
 
 	public override void _Ready()
@@ -21,6 +22,8 @@ public partial class UiBackPackItemList : UiItemList
 
 	private void _buildTitleRow()
 	{
+		ColumnTitlesVisible = true;
+		Columns = ColumnTitles.Count;
 		int i = 0;
 		foreach (var (name, properties) in ColumnTitles) {
 			SetColumnTitle(i, name.ToUpper());
@@ -49,13 +52,16 @@ public partial class UiBackPackItemList : UiItemList
 				: "0";
 
 			//double weight = Math.Round(item.Weight * inventoryItem.Amount, 2);
-
-			row.SetText(0, item.Name);
+			var image = Image.LoadFromFile("res://Assets/Sprites/UI/ui_list_icon.png");
+			row.SetIcon(0, ImageTexture.CreateFromImage(image));
+			row.SetText(1, item.Name);
 			//row.SetText(1, weight.ToString());
-			row.SetText(1, amount);
-			row.SetText(2, item.Value.ToString());
-			row.SetTextAlignment(1, HorizontalAlignment.Right);
+			row.SetText(2, amount);
+			row.SetText(3, item.Value.ToString());
+			row.SetTextAlignment(0, HorizontalAlignment.Left);
+			row.SetTextAlignment(1, HorizontalAlignment.Left);
 			row.SetTextAlignment(2, HorizontalAlignment.Right);
+			row.SetTextAlignment(3, HorizontalAlignment.Right);
 
 			ListItems.Add(inventoryItem, row);
 		}

dac993a4cc470ef00527b00a91f3fb3e884f47f9
Rico <rico@pgml.de>
2025-04-25 14:14:24 +0200

    clean up throw component + added min throw percentage

--- Common/Components/ThrowComponent.cs
+++ Common/Components/ThrowComponent.cs
@@ -6,9 +6,10 @@ public partial class ThrowComponent : Component
 	public ProgressBar ForceProgress { get; set; }
 
 	[Export]
-	public HBoxContainer ThrowForceIndicator { get; set; }
+	public Control ThrowForceIndicator { get; set; }
 
 	public bool CanThrow { get; set; } = false;
+	public float MinThrowPercentage { get; set; } = 20;
 	public float ThrowForce { get; set; } = 0;
 	public float MaxThrowForce { get; set; } = 20;
 	public float ThrowIncreaseStep { get; set; } = 0.05f;
@@ -19,7 +20,6 @@ public partial class ThrowComponent : Component
 	public override void _Ready()
 	{
 		base._Ready();
-
 		ThrowForceIndicator.Visible = false;
 	}
 
@@ -29,48 +29,15 @@ public partial class ThrowComponent : Component
 			if (_chargeProgress <= _throwObject.MaxThrowForce) {
 				_chargeProgress += _throwObject.ThrowIncreaseStep;
 			}
-
-			ThrowForceIndicator.Visible = true;
-			Vector2 mousePos = ThrowForceIndicator.GetGlobalMousePosition();
-			ThrowForceIndicator.GlobalPosition = mousePos - new Vector2(
-				ThrowForceIndicator.Size.X / 2,
-				ThrowForceIndicator.Size.Y + 2
-			);
+			_positionThrowIndicator();
 		}
 
 		if (Input.IsActionJustReleased("action_trigger")) {
-			CanThrow = false;
-			ThrowForce = 0;
-			_chargeProgress = 0;
-
-			if (!_throwObject.IsInsideTree()) {
-				_throwObject.QueueFree();
-			}
-
-			// Reset charge progress ui
-			for (var i = 0; i < 5; i++) {
-				ThrowForceIndicator.GetChild<TextureRect>(i).FlipV = false;
-			}
-
-			ThrowForceIndicator.Visible = false;
+			_resetChargeProgress();
 		}
 
 		if (_throwObject is not null) {
-			ForceProgress.Value = _chargeProgress;
-
-			ProgressBar p = ForceProgress;
-			double percentage = (p.Value - p.MinValue) / (p.MaxValue - p.MinValue) * 100f;
-
-			if (ForceProgress.Value > _throwObject.MinThrowForce &&
-				percentage % 20 == 0 &&
-				percentage <= 100)
-		 	{
-				// update charge progress ui and set throw force
-				for (var i = 0; i < percentage / 20; i++) {
-					ThrowForceIndicator.GetChild<TextureRect>(i).FlipV = true;
-					ThrowForce = _chargeProgress;
-				}
-			}
+			_chargeThrow();
 		}
 	}
 
@@ -82,22 +49,46 @@ public partial class ThrowComponent : Component
 		 	if (mouseEvent.ButtonIndex == MouseButton.Left) {
 				if (ThrowForce > 0) {
 					_throw();
-					ThrowForce = 0;
-					CanThrow = false;
-					ThrowForceIndicator.Visible = false;
+					_resetChargeProgress();
 				}
 			}
 
 		 	if (mouseEvent.ButtonIndex == MouseButton.Right) {
 				_throwObject = _instantiateThrowObject();
-				//ThrowForce = _throwObject.MinThrowForce;
 				ForceProgress.MinValue = _throwObject.MinThrowForce;
 				ForceProgress.MaxValue = _throwObject.MaxThrowForce;
+				// force always a specific amount of initial progress
+				// otherwise if charging and throwing quickly would be just
+				// dropping
+				_chargeProgress = 20f / 100f * MinThrowPercentage;
 				CanThrow = true;
 			}
 		}
 	}
 
+	/// <summary>
+	/// Prepares a throw and determines the ThrowForce based on charge progress
+	/// </summary>
+	private void _chargeThrow()
+	{
+		ForceProgress.Value = _chargeProgress;
+
+		var p = ForceProgress;
+		double percentage = (p.Value - p.MinValue) / (p.MaxValue - p.MinValue) * 100f;
+		percentage = Mathf.Floor(percentage);
+
+		if (percentage % 20 == 0 && percentage <= 100) {
+			// update charge progress ui and set base throw force
+			for (var i = 0; i < percentage / 20; i++) {
+				ThrowForceIndicator.GetChild<TextureRect>(i).FlipV = true;
+				ThrowForce = _chargeProgress;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Throws the selected object
+	/// </summary>
 	private void _throw()
 	{
 		var actor = ActorData.Character<Actor>();
@@ -117,18 +108,53 @@ public partial class ThrowComponent : Component
 		Vector3 rayOrigin = camera.ProjectRayOrigin(mousePos);
 		Vector3 rayDirection = camera.ProjectRayNormal(mousePos).Normalized();
 
+		// determine the throw direction from the origin
+		// based on the position of the mouse cursor
 		float groundY = actor.GlobalPosition.Y;
 		float distanceToIntersection = (groundY - rayOrigin.Y) / rayDirection.Y;
 		Vector3 hitPoint = rayOrigin + rayDirection * distanceToIntersection;
-
 		Vector3 direction = (hitPoint - actor.GlobalPosition).Normalized();
-		float angle = Mathf.DegToRad(60);
-		float verticalComponent = Mathf.Sin(angle) * ThrowForce;
 
-		Vector3 velocity = direction * ThrowForce + new Vector3(0, verticalComponent, 0);
+		// throw at a roughly 45° angle
+		float angleDeg = 45 + (float)GD.RandRange(-5,  5);
+		float angle = Mathf.DegToRad(angleDeg);
+
+		// add some randomness to the throw force otherwise it would always land
+		// on the same spot
+		ThrowForce += (float)GD.RandRange(-0.4, 0.4);
+
+		// same with ThrowForce – add some randomness to the trajectory to vary not
+		// only the distance but also the "wideness"
+		Vector3 trajectory = new Vector3(
+			GD.RandRange(-1, 1),
+		 	Mathf.Sin(angle) * ThrowForce,
+			GD.RandRange(-1, 1)
+		);
+
+		Vector3 velocity = direction * ThrowForce + trajectory;
 		_throwObject.LinearVelocity = velocity;
 
 		_updateInventory();
+	}
+
+	/// <summary>
+	/// Resets a throw/charge and hides charge indicator UI
+	/// </summary>
+	private void _resetChargeProgress()
+	{
+		CanThrow = false;
+		ThrowForce = 0;
+		_chargeProgress = 0;
+
+		if (!_throwObject.IsInsideTree()) {
+			_throwObject.QueueFree();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			ThrowForceIndicator.GetChild<TextureRect>(i).FlipV = false;
+		}
+
+		ThrowForceIndicator.Visible = false;
 	}
 
 	private PackedScene _itemScene()
@@ -145,6 +171,9 @@ public partial class ThrowComponent : Component
 		return _itemScene().Instantiate<PhysicsObject>();
 	}
 
+	/// <summary>
+	/// Updates the amount of the thrown item in the inventory
+	/// </summary>
 	private bool _updateInventory()
 	{
 		var actor = ActorData.Character<Actor>();
@@ -152,5 +181,19 @@ public partial class ThrowComponent : Component
 		int itemResourceIndex = actor.Inventory.GetItemResourceIndex(itemResource);
 
 		return actor.Inventory.RemoveOneItem(itemResourceIndex);
+	}
+
+	/// <summary>
+	/// Shows throw charge indicator and positions it at the mouse cursor
+	/// </summary>
+	private void _positionThrowIndicator()
+	{
+		ThrowForceIndicator.Visible = true;
+		Vector2 mousePos = ThrowForceIndicator.GetGlobalMousePosition();
+
+		ThrowForceIndicator.GlobalPosition = mousePos - new Vector2(
+			ThrowForceIndicator.Size.X / 2,
+			ThrowForceIndicator.Size.Y - 1
+		);
 	}
 }

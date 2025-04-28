24d59de2f1f70b5b9ccc7ee092dbd78a2c895676
Rico <rico@pgml.de>
2025-04-28 15:12:38 +0200

    wip: shadow anomaly, test shaders

---
+++ Anomalies/Scripts/ShadowAnomaly.cs
@@ -0,0 +1,69 @@
+using Godot;
+using System.Collections.Generic;
+
+public partial class ShadowAnomaly : Anomaly
+{
+	[Export]
+	public uint DefaultVisualLayer { get; set; } = 1;
+
+	[Export]
+	public uint AnomalyVisualLayer { get; set; } = 2;
+
+	[Export]
+	public Area3D AnomalyArea { get; set; }
+
+	public async override void _Ready()
+	{
+		base._Ready();
+
+		AnomalyArea.BodyEntered += _onBodyEntered;
+		AnomalyArea.BodyExited += _onBodyExited;
+
+		await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		foreach (var node in _findVisualInstances(this)) {
+			if (node is VisualInstance3D child) {
+				child.Layers = AnomalyVisualLayer;
+			}
+		}
+	}
+
+	private void _anomalyEntered(Node body)
+	{
+		foreach (var node in _findVisualInstances(body)) {
+			if (node is VisualInstance3D child) {
+				child.Layers = AnomalyVisualLayer;
+			}
+		}
+	}
+
+	private void _anomalyExited(Node body)
+	{
+		foreach (var node in _findVisualInstances(body)) {
+			if (node is VisualInstance3D child) {
+				child.Layers = DefaultVisualLayer;
+			}
+		}
+	}
+
+	private List<VisualInstance3D> _findVisualInstances(Node node)
+	{
+		var visualInstances = new List<VisualInstance3D>();
+		foreach (var child in node.GetChildren()) {
+			if (child is VisualInstance3D visualInstance) {
+				visualInstances.Add(visualInstance);
+			}
+			visualInstances.AddRange(_findVisualInstances(child));
+		}
+		return visualInstances;
+	}
+
+	private void _onBodyEntered(Node body)
+	{
+		_anomalyEntered(body);
+	}
+
+	private void _onBodyExited(Node body)
+	{
+		_anomalyExited(body);
+	}
+}

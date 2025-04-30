c16ce98d4ec6ac92e3daccec5b605d9dae5f4697
Rico <rico@pgml.de>
2025-04-30 15:15:34 +0200

    updated warp shader to keep position at mesh base

--- Anomalies/Scripts/Anomaly.cs
+++ Anomalies/Scripts/Anomaly.cs
@@ -3,9 +3,13 @@ using Godot;
 public partial class Anomaly : Node3D
 {
 	protected World World { get; set; }
+	protected CreatureData ActorData;
 
 	public async override void _Ready()
 	{
 		World = GetTree().CurrentScene.FindChild("World") as World;
+
+		await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		ActorData = GD.Load<CreatureData>(Resources.ActorData);
 	}
 }
